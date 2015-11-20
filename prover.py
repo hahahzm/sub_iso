1cdc8d298c768b1d1a5da57aae2d2c1364e7f961
hahahzm <chen1196@live.com>
2015-11-20 18:19:07 -0500

    add prover class

---
+++ prover.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+from thread import *
+
+def threadServer(socket):
+	conn.send('O ha yo u ~\n')
+	while True:
+		data=conn.recv(1024)
+		reply='WTF...' + data
+		if not data:
+			break
+		conn.sendall(reply)
+	conn.close()
+
+
+HOST = ''
+PORT = int(sys.argv[1])
+
+# create socket
+s = socket.socket(
+	socket.AF_INET, socket.SOCK_STREAM)
+
+print s
+
+# bind socket 
+try:
+	s.bind((HOST, PORT))
+except socket.error as errmsg:
+	print 'Binding error:' + str(errmsg[0]) + ':' + errmsg[1]
+	sys.exit()
+
+# listen to socket
+s.listen(10)
+
+#talking to client
+while True:
+	conn, addr = s.accept()
+	print 'Connected with' + addr[0] + ':' + str(addr[1])
+
+	start_new_thread(threadServer, (conn,))
+
+s.close()
